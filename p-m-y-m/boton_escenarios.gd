6781cd184b991c672401c247bbc1fd5623187b30
lauchasilva <lautaro.e.silva03@gmail.com>
2026-05-06 11:13:06 -0300

    script de cambio de escenarios
    Al clickear el botón de mapa, se despliega un panel que nos ofrece clickear distintos botones. Cada uno nos cambiará de escenarios (cuando los haya) y además existe un botón para cerrar el panel del mapa.

---
+++ p-m-y-m/boton_escenarios.gd
@@ -0,0 +1,38 @@
+extends Control
+
+@onready var boton = $MarginContainer/Button
+@onready var menu = $MarginContainer/menu
+
+#acá debería ir la ruta a la escena inicial del juego
+var escena_actual = ""
+
+func _ready():
+	menu.visible = false
+	escena_actual = get_tree().current_scene.scene_file_path
+
+func _on_button_pressed() -> void:
+	menu.visible = true
+
+func cambiar_escena(ruta_escena):
+	if ruta_escena == escena_actual:
+		menu.visible = false
+		return
+	get_tree().change_scene_to_file(ruta_escena)
+
+#acá debería ir la ruta a una escena del juego
+func _on_escenario_1_pressed() -> void:
+	#cambiar_escena("")
+	print("cambiaste a escenario 1")
+
+#acá debería ir la ruta a una escena del juego
+func _on_escenario_2_pressed() -> void:
+	#cambiar_escena("")
+	print("cambiaste a escenario 2")
+
+#acá debería ir la ruta a una escena del juego
+func _on_escenario_3_pressed() -> void:
+	#cambiar_escena("")
+	print("cambiaste a escenario 3")
+
+func _on_salir_pressed() -> void:
+	menu.visible = false
