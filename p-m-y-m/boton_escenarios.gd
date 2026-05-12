b7c3d7d0822e2c653a9e3b2b7d93deddb8140926
Adrian Zalazar <papin_1@hotmail.es>
2026-05-12 12:14:27 -0300

    ensambre de scenas
    se creo el nivel 2 y se conecto cada scena al cambio de nivel en el menu y se agrego el sprite del personaje con animacion

--- p-m-y-m/boton_escenarios.gd
+++ p-m-y-m/boton_escenarios.gd
@@ -23,11 +23,13 @@ func cambiar_escena(ruta_escena):
 func _on_escenario_1_pressed() -> void:
 	#cambiar_escena("")
 	print("cambiaste a escenario 1")
+	get_tree().change_scene_to_file("res://scenas/nivel_1/nivel_1.tscn")
 
 #acá debería ir la ruta a una escena del juego
 func _on_escenario_2_pressed() -> void:
 	#cambiar_escena("")
 	print("cambiaste a escenario 2")
+	get_tree().change_scene_to_file("res://scenas/nivel_2/nivel_2.tscn")
 
 #acá debería ir la ruta a una escena del juego
 func _on_escenario_3_pressed() -> void:
--- p-m-y-m/luther.gd
+++ p-m-y-m/luther.gd
@@ -3,13 +3,26 @@ extends CharacterBody2D
 
 @export var speed: float = 300.0
 @onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+@onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+func _ready() -> void:
+	animation_player.play("idle")
+	pass
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		set_movement_target(get_global_mouse_position())
+		cambio_direccion()
 
 func set_movement_target(target_point: Vector2):
 	nav_agent.target_position = target_point
+	
+func cambio_direccion():
+	if up_direction.x < 0:
+		sprite_2d.flip_h = true
+	else: sprite_2d.flip_h = false
+	pass
 
 func _physics_process(_delta: float) -> void:
 	if nav_agent.is_navigation_finished():
