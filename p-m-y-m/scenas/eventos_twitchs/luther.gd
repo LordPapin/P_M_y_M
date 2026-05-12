0321d8516ea8c25df7c4dcb1a5c0775ae214bd9f
lauchasilva <lautaro.e.silva03@gmail.com>
2026-05-12 13:16:59 -0300

    lengua que se dispara

--- p-m-y-m/scenas/eventos_twitchs/luther.gd
+++ p-m-y-m/scenas/eventos_twitchs/luther.gd
@@ -1,1 +1,16 @@
 extends Node2D
+
+@onready var boca = $boca
+@onready var lengua = $lengua
+
+func _input(event):
+	if event.is_action_pressed("dispararLengua"):
+		disparar_lengua(get_global_mouse_position())
+
+func disparar_lengua(target_pos):
+	lengua.visible = true
+	lengua.clear_points()
+	lengua.add_point(lengua.to_local(boca.global_position))
+	lengua.add_point(lengua.to_local(target_pos))
+	await get_tree().create_timer(0.08).timeout
+	lengua.visible = false
