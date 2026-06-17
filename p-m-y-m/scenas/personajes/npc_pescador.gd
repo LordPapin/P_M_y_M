aee48149650efdc7fe952fc58f3455dacb588b0f
lauchasilva <lautaro.e.silva03@gmail.com>
2026-06-16 23:20:15 -0300

    escena de pescador

---
+++ p-m-y-m/scenas/personajes/npc_pescador.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+var miniJuego = "res://scenas/eventos_twitchs/evento_twitch_1.tscn"
+var conversable = false
+@onready var MiSprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+@onready var MiDialogo = preload ("res://dialogos/npc_barman.dialogue")
+func _ready() -> void:
+	MiSprite.play("idle")
+	conversable = false
+	pass
+
+
+func _handle_interaction():
+	var state = NPCstates.npcs ["npc_barman"]["current_state"]
+	match state:
+		"no_interactuado_sin_billetes":
+			print("diálogo inicial")
+			NPCstates.npcs["npc_barman"]["current_state"] = "interactuado_sin_billetes"
+		"interactuado_sin_billetes":
+			print("dialogo de boludeo")
+		"con_billetes":
+			print("nos dice pobres")
+
+		
+
+func _on_cercanía_de_conv_body_entered(body: Node2D) -> void:
+	if body is personaje:
+		conversable = true
+		print(conversable)
+
+
+func _on_cercanía_de_conv_body_exited(body: Node2D) -> void:
+	if body is personaje:
+		conversable = false
+		print(conversable)
+
+
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton && event.pressed && conversable == true:
+		DialogueManager.show_dialogue_balloon(MiDialogo, "start")
+		await DialogueManager.dialogue_ended
+		_handle_interaction()
