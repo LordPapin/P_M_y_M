4c1ed8bbe70cf2eb9fd10ac2127b6c9a8c3255ff
Adrian Zalazar <papin_1@hotmail.es>
2026-06-19 19:35:48 -0300

    inv
    se agrego el consumo de item y el poder tirarlos

--- p-m-y-m/inventory_ui.gd
+++ p-m-y-m/inventory_ui.gd
@@ -13,11 +13,14 @@ extends CanvasLayer
 
 func _ready():
 	Inventory.inventory_updated.connect(update_slots)
+	
+	# Conectamos cada espacio para que detecte el ratón y le pasamos su número (índice)
+	for i in range(slots.size()):
+		# .bind(i) envía el número del slot (0, 1, 2 o 3) a la función
+		slots[i].gui_input.connect(_on_slot_gui_input.bind(i))
+		
 	update_slots()
-	
-	
-	contenedor_slots.hide() 
-
+	contenedor_slots.hide()
 func update_slots():
 	for i in range(Inventory.MAX_SLOTS):
 		if Inventory.items[i] != null:
@@ -28,3 +31,17 @@ func update_slots():
 
 func _on_maletin_button_pressed():
 	contenedor_slots.visible = !contenedor_slots.visible
+	
+func _on_slot_gui_input(event: InputEvent, index: int):
+	# Detectamos si fue un clic izquierdo presionado
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		
+		# Solo hacemos algo si hay un objeto en ese espacio
+		if Inventory.items[index] != null:
+			print("Sacando objeto del espacio: ", index)
+			
+			# Aquí podrías añadir lógica si quieres soltar el objeto en el mundo de nuevo
+			# ...
+			
+			# Borramos el objeto del inventario
+			Inventory.remove_item(index)
