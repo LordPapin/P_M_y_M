01d99aeccb3bc0c4ce6cc8c331fbc46c837d3d2a
Manzzis <mati.manzano.exe@gmail.com>
2026-06-22 22:51:35 -0300

    pantalla reintentar MJ2
    añadi la pantalla de derrota cada vez que se pierde el minijuego

--- p-m-y-m/scenas/eventos_twitchs/evento_twitch_2_manager.gd
+++ p-m-y-m/scenas/eventos_twitchs/evento_twitch_2_manager.gd
@@ -2,7 +2,6 @@ extends CanvasLayer
 
 @onready var luther = $Luther
 
-signal finalizar
 
 @onready var grafiti1 = $Grafitis/grafiti1
 @onready var grafiti2 = $Grafitis/grafiti2
@@ -11,7 +10,11 @@ signal finalizar
 @onready var laucha_mapache = $LauchaMapache
 @onready var boton = $Boton
 
+@onready var pantalla_derrota = $pantalla_reintentar
+@onready var reintentar_btn = $pantalla_reintentar/reintentar_btn
 var dialogo = load("res://dialogos/escucha_laucha_mapache.dialogue")
+
+var posicion_inicial_luther : Vector2
 
 var fase_actual := 0
 
@@ -22,46 +25,38 @@ var objetivos = [
 ]
 
 func _ready():
-
+	posicion_inicial_luther = luther.global_position
 	luther.grafiti_alcanzado.connect(_on_grafiti_alcanzado)
-
+	
 	laucha_mapache.inicio_vigilancia.connect(_on_inicio_vigilancia)
-	laucha_mapache.alertados.connect(_on_alertados)
-
 	boton.prueba_superada.connect(_on_prueba_superada)
 	boton.prueba_fallida.connect(_on_prueba_fallida)
-
-	# SOLO PARA TEST
+	
+	pantalla_derrota.hide()
+	
 	iniciar_minijuego()
-	print(grafiti1)
-	print(grafiti2)
-	print(grafiti3)
+	
+
 
 func iniciar_minijuego():
-
+	luther.global_position = posicion_inicial_luther
+	pantalla_derrota.hide()
 	fase_actual = 0
-
 	luther.cambiar_estado("caminando")
-
 	luther.ir_a_grafiti(grafiti1, 0)
-
 	laucha_mapache.iniciar_patron()
 
 
 func _on_grafiti_alcanzado(indice):
-
 	print("Llegó al grafiti ", indice)
 
 
 func _on_inicio_vigilancia():
-
 	if fase_actual >= 3:
 		return
-
+		
 	luther.cambiar_estado("invisible")
-
 	boton.global_position = luther.global_position + Vector2(0, 80)
-
 	boton.iniciar_prueba(objetivos[fase_actual])
 
 
@@ -85,10 +80,11 @@ func _on_prueba_fallida():
 
 
 func _on_alertados():
+	luther.destino = null
+	luther.velocity = Vector2.ZERO
+	
+	pantalla_derrota.show()
 
-	print("DERROTA")
-
-	# Aquí pondrás las viñetas
 
 func eliminar():
 	queue_free()
@@ -99,4 +95,9 @@ func _on_finalizar() -> void:
 	print("MINIJUEGO SUPERADO")
 	DialogueManager.show_dialogue_balloon(dialogo)
 	get_tree().call_group("nivel_3","quitar_pausa")
+	await get_tree().create_timer(0.7).timeout
 	eliminar()
+
+
+func _on_reintentar_btn_pressed() -> void:
+	iniciar_minijuego()
--- p-m-y-m/scenas/eventos_twitchs/laucha_mapache.gd
+++ p-m-y-m/scenas/eventos_twitchs/laucha_mapache.gd
@@ -11,52 +11,37 @@ var estado_actual := "susurrando"
 @onready var timer := Timer.new()
 
 func _ready():
-
 	add_child(timer)
-
 	timer.one_shot = true
-
 	timer.timeout.connect(_on_timer_timeout)
 
 
 func iniciar_patron():
-
 	cambiar_estado("susurrando")
 
 
 func cambiar_estado(nuevo_estado:String):
-
 	estado_actual = nuevo_estado
-
 	match estado_actual:
-
 		"susurrando":
-
 			print("Susurrando")
-
 			fin_vigilancia.emit()
-
 			timer.start(tiempo_susurro)
-
 		"vigilando":
 			print("Vigilando")
 			inicio_vigilancia.emit()
-
 		"alertados":
-
 			print("Alertados")
-
 			alertados.emit()
+
 
 func terminar_vigilancia():
 	cambiar_estado("susurrando")
-	
+
+
 func _on_timer_timeout():
-
 	match estado_actual:
-
 		"susurrando":
 			cambiar_estado("vigilando")
-
 		"vigilando":
 			cambiar_estado("susurrando")
