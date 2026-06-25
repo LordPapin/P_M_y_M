09faf16c9c2e5200cd9beb38d55f5c9a66c72a2b
Manzzis <mati.manzano.exe@gmail.com>
2026-06-25 10:11:57 -0300

    dsadsa
    dsadsa

--- p-m-y-m/scenas/nivel_6/banco.gd
+++ p-m-y-m/scenas/nivel_6/banco.gd
@@ -4,7 +4,7 @@ extends Node2D
 func _on_texture_button_pressed() -> void:
 	var distancia : int = $Luther.global_position.distance_to($puerta.global_position)
 	print(distancia)
-	if distancia < 280:
+	if distancia < 350:
 		get_tree().change_scene_to_file("res://scenas/nivel_6/interior_banco.tscn")
 
 
