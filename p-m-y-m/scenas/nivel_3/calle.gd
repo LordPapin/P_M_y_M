bf0ee0104a375b9519723f5b60397f12ac141155
Manzzis <mati.manzano.exe@gmail.com>
2026-06-25 00:51:40 -0300

    termine con los botones y la introduccion
    añadi introduccion de imagenes
    añadi todos los botonones
    añadi boton para volver a la oficina
    añadi boton para ir al banco

--- p-m-y-m/scenas/nivel_3/calle.gd
+++ p-m-y-m/scenas/nivel_3/calle.gd
@@ -14,3 +14,24 @@ func _on_callejon_btn_pressed() -> void:
 	print(distancia)
 	if distancia < 400:
 		get_tree().change_scene_to_file("res://scenas/nivel_2/nivel_2.tscn")
+
+
+func _on_muelle_btn_pressed() -> void:
+	var distancia : int = $Luther.global_position.distance_to($muelle_btn.global_position)
+	print(distancia)
+	if distancia < 400:
+		get_tree().change_scene_to_file("res://scenas/nivel_5/muelle.tscn")
+
+
+func _on_banco_tbn_pressed() -> void:
+	var distancia : int = $Luther.global_position.distance_to($banco_tbn.global_position)
+	print(distancia)
+	if distancia < 400:
+		get_tree().change_scene_to_file("res://scenas/nivel_6/banco.tscn")
+
+
+func _on_oficina_btn_pressed() -> void:
+	var distancia : int = $Luther.global_position.distance_to($oficina_btn.global_position)
+	print(distancia)
+	if distancia < 400:
+		get_tree().change_scene_to_file("res://scenas/nivel_1/nivel_1.tscn")
